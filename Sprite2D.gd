efc23915eb2476fb9aad816e323349a869e5ab2d
MartinCabe <martin.cabrera5980@alumnos.udg.mx>
2024-03-16 11:24:08 -0600

    Initial commit

---
+++ Sprite2D.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var velocidad = 400
+var velocidad_angular = PI
+
+
+func _process(delta):
+	print("Hello, class CUCEI")
+	var direccion = 0
+	if Input.is_action_pressed("ui_left"):
+		direccion = -1
+	if Input.is_action_pressed("ui_right"):
+		direccion = 1
+
+	rotation += velocidad_angular * direccion * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * velocidad
+
+	position += velocity * delta
